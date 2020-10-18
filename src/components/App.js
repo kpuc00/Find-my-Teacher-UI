33d7cf179496fbe7e8b4b2d5499dd31c0cf3280c
Tsvetkov,Alexander A.Z <a.tsvetkov@student.fontys.nl>
2020-10-18 16:46:34 +0200

    Adding followers tab in the index

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/components/App.css';
 import TeacherInfoComponent from "./Vasil/TeacherInfoComponent";
+import FollowersInfoComponent from "./Alexander/FollowersInfoComponent";
 
 function App() {
     return (
         <div className="App">
             <TeacherInfoComponent/>
+            <FollowersInfoComponent/>
         </div>
     );
 }
