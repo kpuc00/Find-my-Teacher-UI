6790f2ee4e3b3d655bbea554ef53143d2caceb85
Rostislav Ivanov <i425588@fhict.nl>
2020-10-20 11:58:38 +0200

    Added favourite teachers list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/components/App.css';
+import FavouriteTeachersComponent from './Rostislav/FavouriteTeachersComponent';
 import TeacherInfoComponent from "./Vasil/TeacherInfoComponent";
 
 function App() {
     return (
         <div className="App">
             <TeacherInfoComponent/>
+            <FavouriteTeachersComponent/>
         </div>
     );
 }
---
+++ src/components/Rostislav/FavouriteTeachersComponent.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import '../../styles/components/Rostislav/favouriteTeachersComponent.css';
+import {
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavItem,
+    MDBNavLink,
+    MDBNavbarToggler,
+    MDBCollapse,
+    MDBContainer
+} from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export default class FavouriteTeachersComponent extends Component {
+
+
+    state = {
+        collapseID: ''
+    };
+
+    toggleCollapse = collapseID => () => {
+        this.setState(prevState => ({
+            collapseID: prevState.collapseID !== collapseID ? collapseID : ''
+        }));
+    };
+
+    render() {
+        const teachers = [
+            {
+                name: 'Michael Koehorst',
+                nowSharing: true
+            },
+
+            {
+                name: 'Flip Wetzer',
+                nowSharing: true
+            },
+            {
+                name: 'Rafayel Avetyan',
+                nowSharing: false
+            }
+        ]
+        teachers.sort(function(a, b){              /* sorts to have teachers that are now sharing location on top */
+            var keyA = a.nowSharing,
+            keyB = b.nowSharing;
+
+            if(keyA == true) return -1;
+            if(keyA == keyB) return -1;
+            if(keyA != keyB) return 1;
+            return 0;
+        });
+
+        let teachersList = teachers.map((d) => {                         /* gets all teachers objects  */
+            if (d.nowSharing)
+                return (<MDBNavItem key={d.name}>
+                    <MDBNavLink to='#!' className="available">
+                        {d.name}
+                    </MDBNavLink>
+                </MDBNavItem>);
+            return (<MDBNavItem key={d.name}>
+                <MDBNavLink to='#!' disabled className="unavailable">
+                    {d.name}
+                </MDBNavLink>
+            </MDBNavItem>)
+        });
+        return (
+            <Router>
+                <MDBContainer id="favourites">
+                    <MDBNavbar
+                        color='light-blue lighten-4'
+                        style={{ marginTop: '20px' }}
+                        light
+                    >
+                        <MDBContainer>
+                            <div className="headOfContainer">
+                                <MDBNavbarBrand>Favourite</MDBNavbarBrand>
+                                <MDBNavbarToggler
+                                    onClick={this.toggleCollapse('navbarCollapse1')}
+                                />
+                            </div>
+                            <MDBCollapse
+                                id='navbarCollapse1'
+                                isOpen={this.state.collapseID}
+                                navbar
+                            >
+                                <MDBNavbarNav >   {/* left */}
+                                    <MDBNavLink to='#!' disabled className="collapsedHeader">
+                                        Teachers
+                                    </MDBNavLink>
+                                    {teachersList}                  {/* foreach teacher in favourites renders */}
+                                    <MDBNavLink to='#!' disabled className="collapsedFooter">
+                                        {teachersList.length}/10
+                                    </MDBNavLink>
+                                </MDBNavbarNav>
+                            </MDBCollapse>
+                        </MDBContainer>
+                    </MDBNavbar>
+                </MDBContainer>
+            </Router>
+        );
+    }
+}
