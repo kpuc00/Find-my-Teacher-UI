7fb37ff370a29300bba439507920422d438ba5b0
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-10-20 16:55:45 +0300

    Added location toggle btn in Teachers view

--- src/components/TeachersView.js
+++ src/components/TeachersView.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/components/App.css';
+import ToggleLocationBtn from './Kris/ToggleLocationBtn';
 
 function TeachersView() {
     return (
         <div className="App">
             <h3>Teachers view</h3>
+            <ToggleLocationBtn />
         </div>
     );
 }
