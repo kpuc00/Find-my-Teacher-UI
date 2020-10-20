2bb3772d3704847e996276e10c9f744059701a89
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-10-20 15:03:09 +0300

    Added Teachers view

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,14 @@ import {
     Route
   } from "react-router-dom";
 import StudentsView from './StudentsView';
-
+import TeachersView from './TeachersView';
 
 function App() {
     return (
         <Router>
         <Switch>
             <Route path="/" exact component={StudentsView}/>
+            <Route path="/teacher" component={TeachersView}/>
         </Switch>
         </Router>
     );
---
+++ src/components/TeachersView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/App.css';
+
+function TeachersView() {
+    return (
+        <div className="App">
+            <h3>Teachers view</h3>
+        </div>
+    );
+}
+
+export default TeachersView;
