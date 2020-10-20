49dde83bcab36c86afa7fa57657595cd42fb5c3f
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-10-20 15:01:03 +0300

    Added Back and Zoom In/Out buttons

--- src/components/StudentsView.js
+++ src/components/StudentsView.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../styles/components/App.css';
 import TeacherInfoComponent from "./Vasil/TeacherInfoComponent";
 import SearchBar from "./Misho/SearchBar"
+import ZoomInOutBtns from './Kris/ZoomInOutBtns';
+import BackBtn from './Kris/BackBtn';
 
 function StudentsView() {
     return (
         <div className="App">
             <SearchBar />
             <TeacherInfoComponent/>
+            <ZoomInOutBtns/>
+            <BackBtn/>
         </div>
     );
 }
