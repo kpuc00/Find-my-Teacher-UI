0bffd6333560a6f7507f106b7db9ef33dff732a1
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-11-18 16:16:56 +0100

    Location on the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,24 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/components/map.css';
 
 
-function Map(props) {
-
+function Map({ data }) {
+    console.log(data)
     return (
         <div>
-            <h3>{props.data.building}</h3>
-            <img src={`https://api.fhict.nl/location/mapimage/EHV/R10/${props.data.currentFloor}`} width="100%" alt="floor map" />
+            <h3>{data.building} - Floor {data.currentFloor}</h3>
+            <h4>You are on the {data.user.currentLocation.floor} floor.</h4>
+            <div className="floor-map">
+                <img src={`https://api.fhict.nl/location/mapimage/EHV/R10/${data.currentFloor}`} style={{ position: "relative" }} width={data.api.image.width} height={data.api.image.height} alt="floor map" />
+                {
+                    (data.currentFloor === data.user.currentLocation.floor) &&
+                    <div className="profilepic" style={{ top: data.user.currentLocation.y, left: data.user.currentLocation.x }}>
+                        <img src={data.user.profilePic} alt="me" />
+                    </div>
+                }
+            </div>
         </div>
     )
-
 }
 
 export default Map
--- src/components/StudentsView.js
+++ src/components/StudentsView.js
@@ -11,9 +11,11 @@ import SearchBar from "./Misho/SearchBar"
 import ZoomInOutBtns from './Kris/ZoomInOutBtns';
 import BackBtn from './Kris/BackBtn';
 
-import  Map from './Map';
+import Map from './Map';
 
+import Axios from "axios"
 import AuthService from "../services/AuthService";
+import authHeader from "../services/auth-header";
 
 
 //Bootstrap
@@ -29,12 +31,81 @@ class StudentsView extends React.Component {
             building: "R10",
             floors: ["BG", "1e", "2e", "3e", "4e"],
             currentFloor: "BG",
-            floorIndex: 0
+            floorIndex: 0,
+            user: {
+                currentLocation: {
+                    floor: "",
+                    x: 0,
+                    y: 0
+                },
+                profilePic: null
+            },
+            api: {
+                mapHierarchyFloor: "EHV>R10>2e",
+                mapCoordinate: {
+                    x: 151.844238,
+                    y: 55.4845145
+                },
+                image: {
+                    width: 2232,
+                    height: 748
+                },
+                floorDimension: {
+                    length: 108.27,
+                    width: 331.36,
+                }
+            }
         }
 
         autoBind(this)
     }
 
+    componentDidMount() {
+        let { api } = this.state
+
+        let f = api.mapHierarchyFloor
+        const floor = f.includes('>') && f.substr(f.lastIndexOf('>') + 1).split(' ')[0]
+
+        let kWidth = api.image.width / api.floorDimension.width
+        let kHeight = api.image.height / api.floorDimension.length
+        let width = kWidth * api.mapCoordinate.x
+        let height = kHeight * api.mapCoordinate.y
+
+        Axios.get("https://api.fhict.nl/pictures/i428100.jpg", { headers: authHeader(), responseType: 'arraybuffer' })
+            .then(data => {
+                const b64Data = btoa(
+                    new Uint8Array(data.data).reduce(
+                        (dataArray, byte) => {
+                            return dataArray + String.fromCharCode(byte);
+                        },
+                        ''
+                    )
+                );
+                const userAvatarData = {
+                    key: 'userAvatar',
+                    value: `data:image/png;base64,${b64Data}`
+                };
+                this.setState({
+                    user: {
+                        ...this.state.user,
+                        profilePic: userAvatarData.value
+                    }
+                })
+            })
+
+
+        this.setState({
+            user: {
+                ...this.state.user,
+                currentLocation: {
+                    floor: floor,
+                    x: width,
+                    y: height
+                }
+            },
+            currentFloor: floor
+        })
+    }
 
     handleFloorChange(action) {
         const length = this.state.floors.length //Length of floor array
@@ -56,9 +127,9 @@ class StudentsView extends React.Component {
 
         //Change state
         this.setState(state => ({
-                floorIndex: index,
-                currentFloor: state.floors[index]
-            })
+            floorIndex: index,
+            currentFloor: state.floors[index]
+        })
         )
     }
 
@@ -99,7 +170,7 @@ class StudentsView extends React.Component {
                     </Col>
                     <Col>
                         <div className="float-right">
-                            <ZoomInOutBtns handleFloorChange={this.handleFloorChange}/>
+                            <ZoomInOutBtns handleFloorChange={this.handleFloorChange} />
                         </div>
                     </Col>
                 </Row>
